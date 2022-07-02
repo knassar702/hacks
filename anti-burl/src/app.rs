3f74064f24287ed96ec46aee3878a2c8210c38e4
Khaled Nassar <knassar702@gmail.com>
2022-07-02 22:17:58 +0200

    auto-format

--- anti-burl/src/app.rs
+++ anti-burl/src/app.rs
@@ -3,35 +3,34 @@ use rayon::prelude::*;
 mod req;
 
 pub struct App {
-    pub input: Vec<String>
+    pub input: Vec<String>,
 }
 
-
 impl App {
-    pub fn start(self,threads: u32) {
+    pub fn start(self, threads: u32) {
         let pool = rayon::ThreadPoolBuilder::new()
             .num_threads(threads.try_into().unwrap())
             .build()
             .unwrap();
-        pool.install(||{
-                    self.input.par_iter().for_each(|line| {
-                            let url = line.trim();
-                            match req::send_request(url) {
-                                Ok(res) => {
-                                    if res.status() <= 300 || res.status() >= 500 {
-                                        println!(
-                                            "{} | {} | {}",
-                                            res.status(),
-                                            res.into_string().unwrap().len(),
-                                            url
-                                        );
-                                    }
-                                }
-                                Err(_err) => {
-                                    //  IGNORE 
-                                }
+        pool.install(|| {
+            self.input.par_iter().for_each(|line| {
+                let url = line.trim();
+                match req::send_request(url) {
+                    Ok(res) => {
+                        if res.status() <= 300 || res.status() >= 500 {
+                            println!(
+                                "{} | {} | {}",
+                                res.status(),
+                                res.into_string().unwrap().len(),
+                                url
+                            );
                         }
-                });
+                    }
+                    Err(_err) => {
+                        //  IGNORE
+                    }
+                }
+            });
         });
     }
 }
--- anti-burl/src/cli.rs
+++ anti-burl/src/cli.rs
@@ -1,12 +1,15 @@
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "anti-burl", about = "Takes URLs on stdin, prints them to stdout if they return a 200 OK. That's literally it")]
+#[structopt(
+    name = "anti-burl",
+    about = "Takes URLs on stdin, prints them to stdout if they return a 200 OK. That's literally it"
+)]
 pub struct Opt {
     #[structopt(short = "t", long = "thread", default_value = "30")]
     pub threads: u32,
 }
 
-pub fn get_args() -> Opt{
+pub fn get_args() -> Opt {
     Opt::from_args()
 }
