195f8d4756eb28602fd593ceb7c258266e7dd3ef
Khaled Nassar <knassar702@gmail.com>
2022-07-02 22:17:34 +0200

    add threads option

--- anti-burl/src/app.rs
+++ anti-burl/src/app.rs
@@ -3,28 +3,35 @@ use rayon::prelude::*;
 mod req;
 
 pub struct App {
-    pub input: Vec<String>,
+    pub input: Vec<String>
 }
 
+
 impl App {
-    pub fn start(self) {
-        self.input.par_iter().for_each(|line| {
-                let url = line.trim();
-                match req::send_request(url) {
-                    Ok(res) => {
-                        if res.status() <= 300 || res.status() >= 500 {
-                            println!(
-                                "{} | {} | {}",
-                                res.status(),
-                                res.into_string().unwrap().len(),
-                                url
-                            );
+    pub fn start(self,threads: u32) {
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(threads.try_into().unwrap())
+            .build()
+            .unwrap();
+        pool.install(||{
+                    self.input.par_iter().for_each(|line| {
+                            let url = line.trim();
+                            match req::send_request(url) {
+                                Ok(res) => {
+                                    if res.status() <= 300 || res.status() >= 500 {
+                                        println!(
+                                            "{} | {} | {}",
+                                            res.status(),
+                                            res.into_string().unwrap().len(),
+                                            url
+                                        );
+                                    }
+                                }
+                                Err(_err) => {
+                                    //  IGNORE 
+                                }
                         }
-                    }
-                    Err(_err) => {
-                        //  IGNORE 
-                    }
-            }
-    });
+                });
+        });
     }
 }
--- anti-burl/src/main.rs
+++ anti-burl/src/main.rs
@@ -1,12 +1,14 @@
 use std::io::{self, BufRead};
 
 mod app;
+mod cli;
 
 fn main() {
+    cli::get_args();
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
     let anti_burl = app::App {
         input: lines.collect::<Result<Vec<_>, _>>().unwrap(),
     };
-    anti_burl.start()
+    anti_burl.start(cli::get_args().threads);
 }
