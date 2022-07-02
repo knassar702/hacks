7a00afe646ba27ae1cf02c3ed1fd6816e756081c
Khaled Nassar <knassar702@gmail.com>
2022-07-02 22:23:41 +0200

    auto-format

--- check-cnames/src/app.rs
+++ check-cnames/src/app.rs
@@ -3,24 +3,29 @@ use trust_dns_resolver::config::*;
 use trust_dns_resolver::Resolver;
 
 pub struct App {
-    pub input: Vec<String>
+    pub input: Vec<String>,
 }
 
-
 impl App {
-    pub fn start(self,threads: u32) {
+    pub fn start(self, threads: u32) {
         let pool = rayon::ThreadPoolBuilder::new()
             .num_threads(threads.try_into().unwrap())
             .build()
             .unwrap();
-        pool.install(||{
-
+        pool.install(|| {
             self.input.par_iter().for_each(|domain| {
-                let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+                let resolver =
+                    Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
                 let response = resolver.lookup_ip(domain).unwrap();
-                println!("{}", response.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","));
+                println!(
+                    "{}",
+                    response
+                        .iter()
+                        .map(|x| x.to_string())
+                        .collect::<Vec<String>>()
+                        .join(",")
+                );
             });
-
         });
     }
 }
--- check-cnames/src/main.rs
+++ check-cnames/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let threads = cli::get_args().threads;
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
-    let cname = app::App{input: lines.map(|x| x.unwrap()).collect()};
+    let cname = app::App {
+        input: lines.map(|x| x.unwrap()).collect(),
+    };
     cname.start(threads);
 }
