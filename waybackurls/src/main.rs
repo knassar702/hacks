fcfcac38f1a1dacc1699285ff0fd6c1d36a4e618
knassar702 <knassar702@gmail.com>
2022-07-19 01:11:20 +0200

    better output

--- waybackurls/src/main.rs
+++ waybackurls/src/main.rs
@@ -23,16 +23,14 @@ fn get_webarchive(query: &str) {
     match ureq::get(&url).call() {
         Ok(res) => {
             let json_body: Vec<WebArchive> = res.into_json().unwrap();
-            let mut urls: Vec<String> = Vec::new();
             json_body.iter().for_each(|item| {
                 item.urls
                     .iter()
                     .filter(|url| !url.contains("original"))
                     .for_each(|y| {
-                        urls.push(y.to_string());
+                        println!("{}", y);
                     });
             });
-            println!("{:?}", urls);
         }
         Err(_err) => {
             // IGNORE
